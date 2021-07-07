27083d0a59cd4ba7839fa6bf0bbd22fe48e9bd29
sakshamnagpal <saksham.nagpal@anz.com>
2021-07-08 00:14:34 +1000

    added comments to controller.py

--- bin/controller.py
+++ bin/controller.py
@@ -1,3 +1,7 @@
+"""
+This file contains the game logic and the controller that plays the game.
+This file's class and methods will be called from the main file
+"""
 from board import GameBoard
 from robot import ToyRobot
 from typing import Optional
@@ -7,14 +11,30 @@ from IntegerValidation import validate_integer
 
 
 class Controller:
-    def __init__(self, board_size=5):
+    """
+    This class acts as the interface for board and robot, combining them together.
+    The class contains logic for playing the game, including interpreting user's commands.
+    """
+    def __init__(self, board_size: int = 5) -> None:
+        """
+        This method creates a game board and the robot. The default
+        size for the game board is 5, however, this can be changed by the user
+        :param board_size: The size of the game board, default value is 5
+        """
         self.board = GameBoard(board_size)
         self.robot = ToyRobot()
         self.valid_commands = ["MOVE", "LEFT", "RIGHT", "REPORT"]
 
-    def execute_command(self, command) -> Optional[str]:
+    def execute_command(self, command: str) -> Optional[str]:
+        """
+        This method interprets and runs a command entered by the user. The
+        details of the commands can be found in the README.md file
+        :param command: The command entered by the user
+        :return: String of the robot if the command is report, none otherwise
+        """
         command_list = command.split(" ")
         valid_faces = [face.name for face in Face]
+        # The place command differs from the others and hence needs to be dealt differently
         if len(command_list) > 1 and command_list[0] == "PLACE":
             try:
                 x_coord, y_coord, face_value = command_list[1].split(",")
@@ -28,6 +48,7 @@ class Controller:
             else:
                 self.robot.place(int(x_coord), int(y_coord), Face[face_value])
                 return None
+        # All the other commands are single word and hence can be dealt with rather similarly
         elif command_list[0] in self.valid_commands:
             if command_list[0] == 'LEFT':
                 self.robot.left()
@@ -39,6 +60,7 @@ class Controller:
                 current_coords = (self.robot.get_x_value(), self.robot.get_y_value())
                 current_face = self.robot.get_face_value()
                 self.robot.move()
+                # Check if the movement will make the robot go off the board, raise an error if that's the case
                 if 0 <= self.robot.get_x_value() < len(self.board) and 0 <= self.robot.get_y_value() < len(self.board):
                     return None
                 else:
@@ -46,13 +68,3 @@ class Controller:
                     raise ToyRobotError
             elif command_list[0] == "REPORT":
                 return str(self.robot)
-
-
-if __name__ == '__main__':
-    controller = Controller()
-    controller.execute_command("PLACE 1,2,EAST")
-    controller.execute_command("MOVE")
-    controller.execute_command("MOVE")
-    controller.execute_command("LEFT")
-    controller.execute_command("MOVE")
-    print(controller.execute_command("REPORT"))
